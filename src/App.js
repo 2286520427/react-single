ceda9f52192aa0ea82756555a042ddf54bc4d80b
2286520427 <2286520427@qq.com>
2020-05-12 16:20:09 +0800

    编写hello world

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <div>
+            <h1>hello world!</h1>
+            <p>今天是第一次使用脚手架，很神奇呢~</p>
+        </div>
     </div>
   );
 }
