9e4109119ecf1002563a1ec25900dfa8435af18c
2286520427 <2286520427@qq.com>
2020-05-14 17:12:43 +0800

    React Hooks

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { Button } from "antd";
 import "./../scss/App.scss";
 import "antd/dist/antd.css";
 
 // 函数式组件
-// export default function App() {
-//   return <div className="container">
-//       <h1>this is Login</h1>
-//       <div className="box">
-//         <p>这是Link方式跳转：</p>
-//         <Link to="/home">Home</Link>
-//         <Link to="/login">Login</Link>
-//       </div>
-//     </div>
-// }
+export default function App() {
+  const [count, setCount]= useState(10);
+  useEffect(()=>{
+    console.log('执行了useEffect');
+    // setCount(100);
+  },[])
+  return <div className="container">
+      <h1>this is App</h1>
+      <div className="box">
+        <p>这是Link方式跳转：</p>
+        <Link to="/home">Home</Link>
+        <Link to="/login">Login</Link>
+      </div>
+      <div className="box">
+          <p>Ract Hooks案例：</p>
+          <h3>当前count次数：{count}</h3>
+          <Button onClick={()=>{setCount(count+1)}}>
+            更新次数
+          </Button>
+        </div>
+    </div>
+}
 
 // 有状态组件
-export default class App extends React.Component {
+/* export default class App extends React.Component {
   handleJump=()=>{
     this.props.history.push('/antd')
   }
@@ -39,3 +51,4 @@ export default class App extends React.Component {
       </div>
   }
 }
+ */
