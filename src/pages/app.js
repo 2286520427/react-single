eaec6cbe06d7e1319c3e093a8433aacb3280daeb
2286520427 <2286520427@qq.com>
2020-05-15 18:00:34 +0800

    综合练习

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,54 +1,15 @@
-import React,{useState, useEffect} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "antd";
-import "./../scss/App.scss";
 import "antd/dist/antd.css";
+import "./../scss/App.scss";
 
 // 函数式组件
-export default function App() {
-  const [count, setCount]= useState(10);
-  useEffect(()=>{
-    console.log('执行了useEffect');
-    // setCount(100);
-  },[])
+export default function App(){
+
   return <div className="container">
-      <h1>this is App</h1>
-      <div className="box">
-        <p>这是Link方式跳转：</p>
-        <Link to="/home">Home</Link>
-        <Link to="/login">Login</Link>
-      </div>
-      <div className="box">
-          <p>Ract Hooks案例：</p>
-          <h3>当前count次数：{count}</h3>
-          <Button onClick={()=>{setCount(count+1)}}>
-            更新次数
-          </Button>
-        </div>
+      <h2>欢迎进行第一次React综合练习</h2>
+        <Link to="/login">去登录</Link>
+        <br/>
+        <Link to="/home">去首页</Link>
     </div>
 }
-
-// 有状态组件
-/* export default class App extends React.Component {
-  handleJump=()=>{
-    this.props.history.push('/antd')
-  }
-  render() {
-    return <div className="container">
-        <h1>this is Login</h1>
-        <div className="box">
-          <p>这是Link方式跳转：</p>
-          <Link to="/antd">Antd</Link>
-          <Link to="/home">Home</Link>
-          <Link to="/login">Login</Link>
-        </div>
-        <div className="box">
-          <p>这是button方式跳转：</p>
-          <Button type="primary" onClick={this.handleJump}>
-            Antd
-          </Button>
-        </div>
-      </div>
-  }
-}
- */
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,42 @@
 import React from "react";
+import {Table} from "antd";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>this is Home</div>;
+  const columns = [
+    {
+      title: '地区',
+      dataIndex: 'area',
+    },{
+      title: 'Java',
+      dataIndex: 'java',
+    },{
+      title: 'PHP',
+      dataIndex: 'php',
+    },{
+      title: 'Node',
+      dataIndex: 'node',
+    },{
+      title: 'JS',
+      dataIndex: 'js',
+    },{
+      title: 'Python',
+      dataIndex: 'python',
+    },
+  ];
+  const data = [];
+  return <div className="home">
+    <h1>语言大数据报告</h1>  
+    <div className="wrap">
+      <nav>
+        <a className="active">语言动态</a>
+        <a>语言地图</a>
+        <a>语言热搜</a>
+        <a>语言播报</a>
+      </nav>
+      <p>数据纯属虚构</p>
+      <Table columns={columns} dataSource={data} bordered/>
+      <Link to="/" className="nav-link">回首页</Link>
+    </div>
+  </div>;
 }
